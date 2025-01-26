ba91141134f2dbecec37cbd2ac313616542cb2cf
leetgrind <leetgrind.org@gmail.com>
2025-01-25 23:11:35 -0500

    first

---
+++ Assigment3Dotnet/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assigment3Dotnet
+{
+    public class StringCompression
+    {
+        public String Compress(String s)
+        {
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ StringCompressionTest/Test1.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Assigment3Dotnet;
+
+namespace StringCompressionTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestCompression()
+        {
+            StringCompression stringCompression = new StringCompression();
+
+            string result = stringCompression.Compress("test");
+
+            Assert.AreEqual("test", result);
+        }
+    }
+}
